18dc79ee0ab26ed1da3440a07013895d24231611
Vitaly Glibin <glibin.v@gmail.com>
2018-12-03 16:59:53 +0300

    try fix free busy

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
 
 setup(
   name='pyexchange',
-  version='0.7-hf11',
+  version='0.7-hf12',
   url='https://github.com/linkedin/pyexchange',
   license='Apache',
   author='Rachel Sanders',
